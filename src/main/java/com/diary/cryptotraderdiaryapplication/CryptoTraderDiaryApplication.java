0ac2cb58f041902df20a473557e538b89911e76c
sebastianslupinski <bezerkey@gmail.com>
2018-10-08 14:22:41 +0200

    first commit

---
+++ src/main/java/com/diary/cryptotraderdiaryapplication/CryptoTraderDiaryApplication.java
@@ -0,0 +1,12 @@
+package com.diary.cryptotraderdiaryapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoTraderDiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoTraderDiaryApplication.class, args);
+	}
+}
