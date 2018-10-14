4644d3514ec0d835eb661cede223fd0efe373cbc
sebastianslupinski <bezerkey@gmail.com>
2018-10-14 13:41:13 +0200

    implement methods in Statistics to show average percentage

--- src/main/java/com/diary/cryptotraderdiaryapplication/models/Statistics.java
+++ src/main/java/com/diary/cryptotraderdiaryapplication/models/Statistics.java
@@ -1,12 +1,14 @@
 package com.diary.cryptotraderdiaryapplication.models;
 
 import java.util.*;
+import java.util.concurrent.TimeUnit;
 
 public class Statistics {
 
-    public static List<Budget> budgets = new ArrayList<>();
+    private static List<Budget> budgets = new ArrayList<>();
 
-    public static List getUniqueBudgets(){
+    //gets unique budgets conditions, unique - only one per day
+    private static List getUniqueBudgets(){
         HashMap<Date, Budget> mapa = new HashMap<>();
         for(Budget budget : budgets){
             if(mapa.containsKey(budget.getActualDate())) {
@@ -24,4 +26,42 @@ public class Statistics {
         Collections.sort(uniqueBudgets);
         return uniqueBudgets;
     }
+
+    //calculate average percentage income or outcome per day
+    public static Double getAveragePercent(){
+        List<Budget> uniqueBudgets = getUniqueBudgets();
+        double sumOfPercent = 0;
+        double average = 0;
+        for(int i = 0 ; i<uniqueBudgets.size() ; i++){
+            if(i==0){
+                continue;
+            }
+            double percent = (uniqueBudgets.get(i).getGeneralBudget()/uniqueBudgets.get(i-1).getGeneralBudget()) *100 -100;
+            sumOfPercent+=percent;
+        }
+
+        long sumOfDays = getDifferenceDays();
+        average = sumOfPercent/sumOfDays;
+        return average;
+    }
+
+    private static long getDifferenceDays() {
+        Date oldestDate = findOldestBudget().getActualDate();
+        Date newestDate = findNewestBudget().getActualDate();
+        long diff = newestDate.getTime() - oldestDate.getTime();
+        return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
+    }
+
+    private static Budget findOldestBudget(){
+        Collections.sort(budgets);
+        System.out.println(budgets.get(0).getActualDate().toString());
+        return budgets.get(0);
+    }
+
+    private static Budget findNewestBudget(){
+        Collections.sort(budgets);
+        System.out.println(budgets.get(budgets.size()-1).getActualDate().toString());
+        return budgets.get(budgets.size()-1);
+    }
+
 }
